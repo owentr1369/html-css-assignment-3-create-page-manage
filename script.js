74f095ea83a90d6d02b1c680924b92b3956c96d3
owentr1369 <tamvaa1306@gmail.com>
2022-06-11 19:02:02 +0700

    button for closing and opening create-tab

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const createBtn = document.getElementById('open-response')
 const createResponseBox = document.getElementById('create-response-box')
 const content = document.getElementById('content')
+const close = document.getElementById('close')
 
 createBtn.addEventListener('click', () => {
     createResponseBox.classList.add('open')
+    createResponseBox.classList.remove('hide')
     content.classList.add('hide')
+    content.classList.remove('open')
+    
 })
-
+close.addEventListener('click', () => {
+    content.classList.add('open')
+    content.classList.remove('hide')
+    createResponseBox.classList.add('hide')
+    createResponseBox.classList.remove('open')
+})
 
