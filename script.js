8fb32726ed13a03963a36acc32899ba3b01afe4b
owentr1369 <tamvaa1306@gmail.com>
2022-06-13 21:30:10 +0700

    delete svg and fix script

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const createBtn = document.getElementById('open-response')
-const createResponseBox = document.getElementById('create-response-box')
-const content = document.getElementById('content')
-const close = document.getElementById('close')
+const createBtn = document.getElementById("open-response");
+const createResponseBox = document.getElementById("create-response-box");
+const content = document.getElementById("content");
+const close = document.getElementById("close");
 
-createBtn.addEventListener('click', () => {
-    createResponseBox.classList.add('open')
-    createResponseBox.classList.remove('hide')
-    content.classList.add('hide')
-    content.classList.remove('open')
-    
-})
-close.addEventListener('click', () => {
-    content.classList.add('open')
-    content.classList.remove('hide')
-    createResponseBox.classList.add('hide')
-    createResponseBox.classList.remove('open')
-})
-
+createBtn.onclick = () => {
+  createResponseBox.classList.add("open");
+  createResponseBox.classList.remove("hide");
+  content.classList.add("hide");
+  content.classList.remove("open");
+};
+close.onclick = () => {
+  content.classList.add("open");
+  content.classList.remove("hide");
+  createResponseBox.classList.add("hide");
+  createResponseBox.classList.remove("open");
+};
